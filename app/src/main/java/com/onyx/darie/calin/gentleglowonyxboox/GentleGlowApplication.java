3bb7f505368549b877924687d319f896dfc79a92
Calin Darie <gentle-glow-onyx-boox@user.noreply.github.com>
2021-05-04 07:31:04 +0300

    rename application class

--- app/src/main/java/com/onyx/darie/calin/gentleglowonyxboox/GentleGlowApplication.java
+++ app/src/main/java/com/onyx/darie/calin/gentleglowonyxboox/GentleGlowApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 
-public class SampleApplication extends Application {
+public class GentleGlowApplication extends Application {
 
     @Override
     protected void attachBaseContext(Context base) {
